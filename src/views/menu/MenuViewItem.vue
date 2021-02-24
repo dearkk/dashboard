9291d1fa827a9cfa81fa34de68f5bdff1182a399
huangkun <huangkun@126.com>
2021-02-25 00:20:01 +0800

    first commit

--- src/views/menu/MenuViewItem.vue
+++ src/views/menu/MenuViewItem.vue
@@ -5,30 +5,29 @@
         <span style="margin-left: 3px">{{menuItem.meta.title}}</span>
       </el-link>
     </div>
-    <div class="leve2" v-if="menuItem.children.length > 0">
+    <div class="leve2 clearFixed" v-if="menuItem.children.length > 0">
       <ul>
         <li v-for="(item, index) in menuItem.children" :key="index">
-          <el-popover
-              v-if="item.children !== undefined"
-              placement="right-start"
-              trigger="hover">
-            <a slot="reference" href="#">
+          <div v-if="item.children !== undefined" >
+            <a class="a1 clearFixed" href="#" @mouseover="hover(index)" @mouseleave="leave()">
               <div style="width: 80%">
                 {{item.meta.title}}
               </div>
               <div style="width: 20%;">
                 <i class="el-icon-arrow-right" style="float: right"></i>
               </div>
+              <div v-if="item.children.length > 0" class="leve3" v-show="itemIndex === index">
+                <ul class="clearFixed" style="list-style-type: none">
+                  <li v-for="(item2, index2) in item.children" :key="index2">
+                    <div style="padding-left: 10px; padding-top: 5px;">
+                      <a class="a2" @click="onClick(item2.meta.title)">{{item2.meta.title}}</a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </a>
-            <div v-if="item.children.length > 0">
-              <ul style="list-style-type: none">
-                <li v-for="(item2, index2) in item.children" :key="index2">
-                  {{item2.meta.title}}
-                </li>
-              </ul>
-            </div>
-          </el-popover>
-          <a v-else href="#">
+          </div>
+          <a class="a1" v-else href="#">
             <div style="width: 80%">
               {{item.meta.title}}
             </div>
@@ -50,13 +49,26 @@ export default {
   data() {
     return {
       //menuItem:menuDir.menu[0]
+      itemIndex:-1
     }
   },
   mounted () {
     console.log("menumItem:" + this.menuItem.meta.title)
     console.log("menumItem length:" + this.menuItem.children.length)
 
+  },
+  methods: {
+    hover: function(index) {
+      this.itemIndex = index
+    },
+    leave: function () {
+      this.itemIndex = -1
+    },
+    onClick: function (item) {
+      console.log(item)
+    }
   }
+
 }
 </script>
 
@@ -77,20 +89,39 @@ export default {
   margin-left: 18px;
   font-size: 14px;
 
-  a {
+  .a1 {
     height: 30px;
     font-size: 13px;
     /*实现垂直居中*/
     display: flex;
     align-items: center;
     text-decoration: none;
+    color: #404040;
     i {
       margin-right: 0px;
     }
-
     &:hover {
-      color: #888888;
+      color: #4784d7;
     }
   }
+}
+
+.a2 {
+  color: #606060;
+  &:hover {
+    color: #4784d7;
+  }
+}
+
+.leve3 {
+  margin-right: -202px;
+  margin-top: 50px;
+  border-radius: 1.8px;
+  border:1px solid #f5f5f5;
+  background: #ffffff;
+  width: 200px;
+  min-height: 80px;
+  float: right;
+  box-shadow: 2px 2px 3px 1px #e9e9e9;
 }
 </style>
